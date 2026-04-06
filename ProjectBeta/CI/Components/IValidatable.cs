e908cc649a311639464a4d02a47905f1c8750764
Floris28 <1103037@hr.nl>
2026-04-06 17:09:57 +0200

    Revert "Booking, Receipt"

--- ProjectBeta/CI/Components/IValidatable.cs
+++
@@ -1,7 +0,0 @@
-namespace ProjectBeta.CI.Components;
-
-public interface IValidatable
-{
-    List<string> Validate();
-    void SetErrors(List<string> errors);
-}
--- ProjectBeta/Model/Receipt.cs
+++
@@ -1,9 +0,0 @@
-namespace ProjectBeta.Model;
-
-public class Receipt
-{
-    public int Id { get; set; }
-    public int Booking_ID { get; set; }
-    public decimal Total { get; set; }
-    public DateTime CreatedAt { get; set; }
-}
